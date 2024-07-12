7b126692fafbc8480f7f2430e424a1dbdbfed4a9
Sung <sung272012@hotmail.com>
2024-07-12 01:19:09 -0400

    Finish sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(firstInteger, secondInteger) {
+    let sum = 0;
+    if (firstInteger < 1 || typeof firstInteger !== "number" || typeof secondInteger !== "number") {
+        return "ERROR";
+    } 
+    if (firstInteger < secondInteger) {
+        while (firstInteger <= secondInteger) {
+            sum += firstInteger;
+            firstInteger++;
+        }
+    } else {
+        while (secondInteger <= firstInteger) {
+            sum += secondInteger;
+            secondInteger++;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
