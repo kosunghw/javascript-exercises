f28351fd1f4772e6cecb7ed510a67c72995c697b
Sung <sung272012@hotmail.com>
2024-07-15 21:37:46 -0400

    submit answers

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,41 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
+    if(a < 0) return "OOPS";
+    if (+a === 0) {
+        return 0;
+    } else if(+a === 1 || +a === 2) {
+        return 1;
+    } else {
+        return fibonacci(+a - 2) + fibonacci(+a - 1);
+    }
 
 };
+
+// Another solution:
+// const fibonacci = function(countArg) {
+//     // checks argument's type and makes sure we use 
+//     // a number throughout rest of function.
+//     let count
+//     if (typeof countArg !== 'number') {
+//         count = parseInt(countArg)
+//     } else {
+//         count = countArg
+//     }
+
+//     if (count < 0) return "OOPS";
+//     if (count == 0) return 0;
+
+//     let firstPrev = 1;
+//     let secondPrev = 0;
+
+//     for (let i = 2; i <= count; i++) {
+//         let current = firstPrev + secondPrev;
+//         secondPrev = firstPrev;
+//         firstPrev = current;
+//     }
+
+//     return firstPrev;
+
+// };
 
 // Do not edit below this line
 module.exports = fibonacci;
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
 
+    arr.map(item=>{
+        if(!item.yearOfDeath){
+            item.age = (new Date()).getFullYear() - item.yearOfBirth;
+        } else {
+            item.age = item.yearOfDeath - item.yearOfBirth;
+            return item;
+        }
+    })
+
+    const sortedArray =  arr.sort((a,b) => {
+        return a.age > b.age ? -1 : 1;
+    })[0];
+
+    return sortedArray;
 };
 
 // Do not edit below this line
